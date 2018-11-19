d4ead8930ecc8be29a4ed7085af4ed19a4d0879c
Derrick Yam <keatkeat87@live.com>
2018-11-19 21:33:21 +0800

    ok

---
+++ src/app/product/product.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.scss']
+})
+export class ProductComponent implements OnInit {
+
+  constructor() { }
+
+  opened = true;
+  mode = 'side';
+
+  ngOnInit() {
+    // this.opened = true;
+  }
+
+
+
+}
